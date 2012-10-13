ee404445a2fcbfd638905d0ad8afe10bbabdc899
zugzug90 <mini-bfg@mail.ru>
2012-10-14 01:54:08 +0700

    Adding SQL-scripts from Lesson 10

---
+++ 10_1.sql
@@ -0,0 +1,1 @@
+﻿SELECT T.surname,T."name",T.last_name, "group"."name" from "group",(SELECT *FROM student WHERE student.IS_HEAD = TRUE) AS T WHERE T.group_id = "group".id ORDER BY T.surname, T."name", T.last_name ASC;
---
+++ 10_3.sql
@@ -0,0 +1,10 @@
+﻿SELECT intersum."name",intersum.q, s1.surname, s1.name, s1.last_name  FROM student AS s1 INNER JOIN
+
+(SELECT "group"."name", "group".id, total.q FROM "group", (SELECT COUNT(s.id) AS q,
+g.id AS gid FROM student AS s INNER JOIN "group" AS g ON (s.group_id=g.id)
+GROUP BY (g.id)) AS total WHERE "group".id = gid) AS intersum
+ON intersum.id = s1.group_id WHERE (s1.is_head = true) 
+
+--  intersum IS subQuery |name|id|total| for each group
+-- s1 IS subTable from student
+-- total is Aggregating GroupBy+Count()
