b4820ba9ba64c133b92a0f1b3e09c826d80450b8
zugzug90 <mini-bfg@mail.ru>
2012-10-17 19:23:23 +0700

    Re-converted 'insert'-scripts to UTF-8 encode.

---
+++ UTF-8/insert_into_group.sql
@@ -0,0 +1,18 @@
+﻿INSERT INTO "group"(id,"name", fac_id, set_year) VALUES
+(1,'ИВТ-148',1,2008
+);
+INSERT INTO "group"(id,"name", fac_id, set_year) VALUES
+(2,'АС-111',1,2011
+);
+INSERT INTO "group"(id,"name", fac_id, set_year) VALUES
+(3,'КБР-110',2,2010
+);
+INSERT INTO "group"(id,"name", fac_id, set_year) VALUES
+(4,'НИП-118',3,2008
+);
+INSERT INTO "group"(id,"name", fac_id, set_year) VALUES
+(5,'НИП-128',3,2008
+);
+INSERT INTO "group"(id,"name", fac_id, set_year) VALUES
+(6,'ДМ-116',4,2006
+);
---
+++ UTF-8/insert_into_student.sql
@@ -0,0 +1,119 @@
+﻿INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(1,1,1,'Ватулина','Ирина','Борисовна',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(2,1,1,'Верховин','Никита','Викторович',TRUE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(3,1,1,'Викулов','Егор','Олегович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(4,1,1,'Горев','Александр','Васильевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(5,1,1,'Иванцов','Сергей','Андреевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(6,1,1,'Корнач','Максим','Анатольевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(7,1,1,'Леонов','Евгений','Андреевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(8,1,1,'Пономарёв','Дмитрий','Дмитриевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(9,1,1,'Пузиков','Александр','Юрьевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(10,1,1,'Сергеев','Дмитрий','Игоревич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(11,1,1,'Сидоров','Сергей','Александрович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(12,1,1,'Чернов','Николай','Анатольевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(13,1,1,'Канаева','Анна','Викторовна',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(14,1,1,'Казаков','Вячеслав','Владимирович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(15,1,1,'Турганов','Рустам','Муратович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(16,1,1,'Чешегоров','Дмитрий','Александрович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(17,1,1,'Усов','Михаил','Александрович',FALSE);
+--
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(18,3,5,'Карбышев','Антон','Константинович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(19,3,5,'Люля','Светлана','Марковна',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(20,3,5,'Багаметов','Дархан','Абаевич',TRUE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(21,3,5,'Першин','Игорь','Андреевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(22,3,5,'Першина','Оксана','Андреевна',FALSE);
+--
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(23,7,6,'Пискун','Татьяна','Владимировна',TRUE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(24,7,6,'Боев','Артём','Павлович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(25,7,6,'Боев','Константин','Николаевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(26,7,6,'Иванов','Игорь','Денисович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(27,7,6,'Кобзин','Александр','Сергеевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(28,7,6,'Лаптев','Александр','Константинович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(29,7,6,'Модин','Адександр','Васильевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(30,7,6,'Титова','Александра','Игоревна',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(31,7,6,'Пыльный','Стас','Юрьевич',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(32,7,6,'Крештель','Зинаида','Николаевна',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(33,7,6,'Груб','Глеб','Степанович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(34,7,6,'Алеев','Рустем','Мурадович',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(35,1,2,'Борщ','Юлия','Александровна',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(36,1,2,'Шопарева','Зоя','Дмитриевна',FALSE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(37,1,2,'Тыщенко','Ольга','Владимировна',TRUE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(38,5,3,'Крюков','Григорий','Федорович',TRUE);
+INSERT INTO student(id,department_id, group_id, surname,
+"name",last_name,is_head) VALUES
+(39,5,3,'Юн','Пек','Фать',FALSE);
