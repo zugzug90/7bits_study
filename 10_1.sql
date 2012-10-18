47714257292ce487fe9f718b6c2f7a46730b2784
zugzug90 <mini-bfg@mail.ru>
2012-10-18 14:53:57 +0700

    Inner-formatting of queries 10_1-10_4 for better reading

--- 10_1.sql
+++ 10_1.sql
@@ -1,1 +1,4 @@
-﻿SELECT T.surname,T."name",T.last_name, "group"."name" from "group",(SELECT *FROM student WHERE student.IS_HEAD = TRUE) AS T WHERE T.group_id = "group".id ORDER BY T.surname, T."name", T.last_name ASC;
+﻿SELECT T.surname,T."name",T.last_name, "group"."name" from "group",
+	(SELECT *FROM student WHERE student.IS_HEAD = TRUE) AS T
+WHERE T.group_id = "group".id 
+ORDER BY T.surname, T."name", T.last_name ASC;
--- 10_2.sql
+++ 10_2.sql
@@ -1,1 +1,4 @@
-﻿SELECT department.title AS "Пустые Кафедры"  FROM department WHERE department.id  NOT IN (SELECT DISTINCT department_id FROM student);
+﻿SELECT department.title AS "Пустые Кафедры"  FROM department
+WHERE department.id  
+	NOT IN 
+	(SELECT DISTINCT department_id FROM student);
